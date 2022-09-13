86aa4d9a07c4af486381bde4b9b24bbf3032327a
Imre <dan.imre.17f@gmail.com>
2022-09-13 18:40:55 +0200

    2022.09.13 18:40

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScript : MonoBehaviour
+{
+    //given:
+    public GameObject OrarendRepresentation_GO;
+    public GameObject OrarendUpload_GO;
+    public GameObject Settings_GO;
+
+    //local:
+    private bool OrarendAlreadyUploaded = false;
+
+    public void orarendButtonPressed()
+    {
+        if (OrarendAlreadyUploaded)
+            OrarendRepresentation_GO.SetActive(true);
+        else
+            OrarendUpload_GO.SetActive(true);
+        
+    }
+
+    public void settingsButtonPressed()
+    {
+
+    }
+}
